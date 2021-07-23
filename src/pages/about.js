857f33da659947b242196e2e0ef58c66fd6daa20
sreejith puthenveetil <sreejith puthenveetil>
2021-07-23 11:54:18 +0530

    we are in business

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is an About GATSBY page.</h1>
+        </div>
+    );
+};
+
+export default About;
